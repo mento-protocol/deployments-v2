67eb2c040b78d41376d3e14c814dab06d12a225d
Nelson Taveras <4562733+nvtaveras@users.noreply.github.com>
2026-03-19 12:58:01 +0100

    fix: other fixes

--- script/deploy/oracles/DeployMockAggregatorReporter.s.sol
+++ script/deploy/oracles/DeployMockAggregatorReporter.s.sol
@@ -6,23 +6,22 @@ import {TrebScript} from "lib/treb-sol/src/TrebScript.sol";
 import {Senders} from "lib/treb-sol/src/internal/sender/Senders.sol";
 import {Deployer} from "treb-sol/src/internal/sender/Deployer.sol";
 
-import {Config, IMentoConfig} from "script/config/Config.sol";
 import {IOwnable} from "mento-core/interfaces/IOwnable.sol";
 
 import {MockAggregatorReporter} from "src/MockAggregatorReporter.sol";
+import {Config, IMentoConfig} from "script/config/Config.sol";
 
 contract DeployMockAggregatorReporter is TrebScript {
     using Deployer for Senders.Sender;
     using Deployer for Deployer.Deployment;
     using Senders for Senders.Sender;
 
-    /// @custom:senders deployer, reporter
+    /// @custom:senders deployer
     function run() public broadcast {
-        Senders.Sender storage deployer = sender("deployer");
-        Senders.Sender storage reporter = sender("reporter");
-
         IMentoConfig config = Config.get();
-        address reporterEOA = reporter.account;
+        Senders.Sender storage deployer = sender("deployer");
+
+        address reporterEOA = config.mockAggregatorReporter();
 
         address reporterAddy =
             deployer.create3("MockAggregatorReporter").deploy(abi.encode(deployer.account, reporterEOA));
--- script/deploy/oracles/DeployMockAggregators.s.sol
+++ script/deploy/oracles/DeployMockAggregators.s.sol
@@ -7,6 +7,7 @@ import {Senders} from "lib/treb-sol/src/internal/sender/Senders.sol";
 import {Deployer} from "treb-sol/src/internal/sender/Deployer.sol";
 
 import {MockChainlinkAggregator} from "src/MockChainlinkAggregator.sol";
+import {MockAggregatorReporter} from "src/MockAggregatorReporter.sol";
 
 import {Config, IMentoConfig} from "script/config/Config.sol";
 
@@ -20,16 +21,24 @@ contract DeployMockAggregators is TrebScript {
         IMentoConfig config = Config.get();
         Senders.Sender storage deployer = sender("deployer");
 
+        // Deploy MockAggregatorReporter if it doesn't exist yet
+        address reporterContract = lookup("MockAggregatorReporter");
+        if (reporterContract == address(0)) {
+            address reporterEOA = config.mockAggregatorReporter();
+            reporterContract = deployer.create3("MockAggregatorReporter").deploy(
+                abi.encode(deployer.account, reporterEOA)
+            );
+            console.log("MockAggregatorReporter deployed at:", reporterContract);
+        }
+
+        // Deploy mock aggregators and wire them to the reporter contract
         IMentoConfig.MockAggregatorConfig[] memory aggConfigs = config.getMockAggregatorConfigs();
-        address reporterContract = config.mockAggregatorReporter();
 
         for (uint256 i = 0; i < aggConfigs.length; i++) {
             address aggAddy = deployer.create3("MockChainlinkAggregator").setLabel(aggConfigs[i].label)
                 .deploy(abi.encode(aggConfigs[i].description, aggConfigs[i].decimals, deployer.account));
             MockChainlinkAggregator agg = MockChainlinkAggregator(deployer.harness(aggAddy));
-            if (reporterContract != address(0)) {
-                agg.setExternalProvider(reporterContract);
-            }
+            agg.setExternalProvider(reporterContract);
             agg.report(aggConfigs[i].initialReport, block.timestamp);
         }
     }
